3d07c0ea43604ebb129cf74b121df766f88addd1
Uyoyoumena Orona <uyoyoumena.orona@optimusbank.com>
2025-02-05 16:57:51 +0100

    reactttt

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBarMain from './components/layout/navbar'
+
+function About() {
+  return (
+    <div className=''>
+    <NavBarMain/> 
+    <h1> All about React </h1>
+    <p>
+      New New new
+    </p>
+    </div>
+    
+  )
+}
+
+
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const[ counter, setCounter] = useState(0);
+
+  const add =()=>{
+    const addedNumber = counter + 1
+    setCounter(addedNumber)
+  }
+  return (
+    <div className='App'>
+      <button onClick={add}>+</button>
+      <p>{counter}</p>
+      <button onClick={()=> setCounter((count) => {
+      if(count >= 1){
+        return count - 1;
+      }
+      return count;
+    })}>-</button>
+    </div>
+    
+  );
+}
+
+export default App;
